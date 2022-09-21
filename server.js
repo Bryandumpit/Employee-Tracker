9f775af39a955ab241f8ae31231520264d5c9c8c
Bryan Dumpit <dumpitbr@gmail.com>
2022-09-21 01:16:35 -0400

    add switch statement based on user choice, base functions for each choice, add add.js and view.js for modularization later

--- server.js
+++ server.js
@@ -8,17 +8,71 @@ const employee = (answer) => {
             name: 'choice',
             message: 'Select an option',
             choices:[
-                'view all departments',
-                'view all roles',
-                'view all employees',
-                'add a department',
-                'add a role',
-                'add an employee',
-                'quit'
+                'View All Departments',
+                'View All Roles',
+                'View All Employees',
+                'Add a Department',
+                'Add a Role',
+                'Add an Employee',
+                'Quit'
             ]
         }
     ])
+    //switch statement based on user choice
+    .then(
+        answer => {
+            switch(answer.choice){
+                case 'View All Departments':
+                    viewDepartments();
+                    break;
+                case 'View All Roles':
+                    viewRoles();
+                    break;
+                case 'View All Employees':
+                    viewEmployees();
+                    break;
+                case 'Add a Department':
+                    addDepartment();
+                    break;
+                case 'Add a Role':
+                    addRole();
+                    break;
+                case 'Add an Employee':
+                    addEmployee();
+                    break;
+                case 'Quit':
+                    quit();
+            }
+        }
+    )
 }
+
+
+//define functions below; modularize later
+function viewDepartments () {
+    console.log('View Deparments');
+}
+
+function viewRoles () {
+    console.log('View Roles');
+}
+
+function viewEmployees () {
+    console.log('View Employees');
+}
+
+function addDepartment () {
+    console.log('Add Department');
+}
+
+function addRole () {
+    console.log('Add Role');
+}
+
+function addEmployee () {
+    console.log('Add Employee');
+}
+//define functions above; modularize later
 
 function init () {
     employee()
